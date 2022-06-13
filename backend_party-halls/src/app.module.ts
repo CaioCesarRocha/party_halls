7fe0ced901df9cdace61442e0635aa5cac003cfe
CaioCesarRocha <63120131+CaioCesarRocha@users.noreply.github.com>
2022-06-13 14:34:46 -0300

    Adicionado backend da aplicação (Nest)

---
+++ backend_party-halls/src/app.module.ts
@@ -0,0 +1,20 @@
+import { Module } from '@nestjs/common';
+import { PhotosModule } from './photos/photos.module';
+import { MongooseModule } from '@nestjs/mongoose';
+import { ConfigModule } from '@nestjs/config';
+
+@Module({
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+    PhotosModule,
+    MongooseModule.forRoot(process.env.MONGODB_HOST, {
+      dbName: 'PartyHalls_NEST',
+      dbTable: 'Photos',
+    }),
+  ],
+  controllers: [],
+  providers: [],
+})
+export class AppModule {}
